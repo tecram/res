07c747ce760389b90df50af501b09c3cab22a2cf
designfenix <info@design-fenix.com.ar>
2017-06-10 18:19:50 -0300

    finish animal area map

--- cms/wp-content/themes/res/page-nosotros.php
+++ cms/wp-content/themes/res/page-nosotros.php
@@ -13,6 +13,7 @@
  */
 
 get_header(); ?>
+
 <section class="engine"><a rel="external" href="https://mobirise.com">free mobile web page builder</a></section><section class="mbr-section mbr-section-hero mbr-section-full mbr-section-with-arrow mbr-after-navbar" id="header1-1" style="background-image: url(<?php echo DIR; ?>/assets/images/nosotros-2000x1333-76.png);">
 
     
@@ -152,14 +153,35 @@ get_header(); ?>
 
 <section class="mbr-section mbr-section__container" id="image2-o" style="background-color: rgb(255, 255, 255); padding-top: 0px; padding-bottom: 0px;">
     <div class="container">
-        <div class="row">
-            <div class="col-xs-12">
-                <figure class="mbr-figure">
-                    <div><img src="<?php echo DIR; ?>/assets/images/cortes-1400x452-84.png"></div>
-
-                    
-
-                </figure>
+        <div class="meat-module">
+            <div class="row">
+                <div class="col-md-6">
+                    <img width="540" height="350" src="<?php echo DIR; ?>/assets/images/cow-plane.jpg" usemap="#cow-map">
+                    <map name="cow-map" id="cow-map">
+                        <area target="" data-id="1" href="" coords="152,98,144,104,134,108,127,101,123,93,125,84,133,74,141,60,145,45,147,33,158,41,173,41,183,39,178,52,174,68,169,78,165,86,160,92" shape="poly">
+                        <area target="" data-id="2" title="2-Palomita" href="" coords="154,100,157,111,161,121,163,129,158,141,149,151,142,163,151,200,151,210,131,185,123,156,121,132,120,118,117,109,118,100,119,89,135,109,146,107" shape="poly">
+                        <area target="" data-id="3" href="" coords="180,71,190,75,198,74,208,72,218,70,224,62,225,51,219,44,205,42,195,38,186,38,178,50,175,62,173,67" shape="poly">
+                        <area target="" data-id="4" href="" coords="222,72,218,78,211,89,212,100,212,113,215,126,219,139,220,153,221,161,218,188,208,193,210,200,202,208,193,205,170,197,157,200,151,193,148,184,144,173,146,161,152,151,159,141,166,129,164,119,159,107,158,98,163,91,169,80,174,70,190,77" shape="poly">
+                        <area target="" data-id="5" href="" coords="182,274,184,262,190,250,195,231,201,220,206,209,184,203,172,199,162,201,152,204,159,225,162,239,162,247,163,261,164,269,170,271" shape="poly">
+                        <area target="" data-id="6" href="" coords="224,68,236,71,252,73,272,76,302,80,322,87,330,88,335,78,305,72,296,67,300,59,307,59,330,59,342,62,346,50,286,54,252,50,227,47" shape="poly">
+                        <area target="" data-id="7" href="" coords="242,73,242,95,237,119,233,140,228,156,229,170,240,150,250,131,254,115,255,93,256,77" shape="poly">
+                        <area target="" data-id="8" href="" coords="223,71,231,72,238,74,262,78,296,82,314,85,327,90,327,98,323,105,334,119,332,153,325,166,317,176,302,170,279,166,262,163,244,160,237,160,222,156,215,114,212,91" shape="poly">
+                        <area target="" data-id="9" href="" coords="223,159,254,162,292,169,318,178,340,191,362,191,340,202,322,215,287,213,247,210,233,216,218,222,211,220,208,211,210,193,220,186" shape="poly">
+                        <area target="" data-id="10" href="" coords="342,62,320,60,304,61,301,67,306,70,314,72,323,73,331,75,338,76,351,78,360,79,374,77,380,68,380,61,373,56" shape="poly">
+                        <area target="" data-id="11" href="" coords="332,89,337,80,352,79,364,79,380,72,381,60,375,53,359,56,344,59,346,47,383,43,396,42,392,55,390,66,396,80,405,86,396,95,380,86,357,88" shape="poly">
+                        <area target="" data-id="12" href="" coords="330,90,342,89,366,88,379,86,395,95,388,113,390,136,391,157,403,181,417,196,431,224,420,221,389,195,365,188,352,188,338,190,319,176,331,160,333,140,335,117,326,105" shape="poly">
+                        <area target="" data-id="13" href="" coords="406,90,417,92,430,91,442,92,454,97,468,101,467,89,464,76,460,63,456,47,446,37,434,39,419,40,398,43,392,57,391,67,398,79" shape="poly">
+                        <area target="" data-id="14" href="" coords="394,144,402,145,406,148,411,157,416,163,423,164,436,169,439,180,451,174,460,169,460,156,465,140,467,115,467,102,444,95,427,92,411,92,403,89,395,100,390,112,391,131" shape="poly">
+                        <area target="" data-id="15" href="" coords="416,189,423,188,432,187,438,181,435,171,428,167,420,164,413,161,407,152,402,145,395,145,392,149,394,160,400,173,405,182" shape="poly">
+                        <area target="" data-id="16" href="" coords="443,239,449,235,456,231,462,229,467,223,466,210,463,197,461,186,460,168,454,175,444,180,437,185,423,189,414,190,431,221" shape="poly">
+                    </map>
+                </div>
+                <div class="col-md-6">
+                    <div class="cow-detail">
+                        <h2>Cortes</h2>
+                        <div class="content"></div>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
@@ -177,14 +199,32 @@ get_header(); ?>
 
 <section class="mbr-section mbr-section__container" id="image2-r" style="background-color: rgb(255, 255, 255); padding-top: 0px; padding-bottom: 0px;">
     <div class="container">
-        <div class="row">
-            <div class="col-xs-12">
-                <figure class="mbr-figure">
-                    <div><img src="<?php echo DIR; ?>/assets/images/cortescerdo-1400x452-90.png"></div>
-
-                    
-
-                </figure>
+        <div class="meat-module">
+            <div class="row">
+                <div class="col-md-6">
+                    <img width="540" height="350" src="<?php echo DIR; ?>/assets/images/oink-plane.jpg" usemap="#oink-map">
+                    <map name="oink-map" id="oink-map">
+                        <area target="" data-id="1" title="1" href="" coords="154,71,164,84,185,112,201,138,230,128,249,121,244,103,232,71,223,44,196,48,172,57" shape="poly">
+                        <area target="" data-id="2" title="2" href="" coords="224,43,231,65,240,90,252,123,283,113,325,109,371,107,380,107,386,87,392,76,401,66,411,59,420,52,429,50,397,44,366,45,334,42,298,38,259,42" shape="poly">
+                        <area target="" data-id="3" title="3" href="" coords="203,138,206,144,211,154,219,150,231,148,239,151,246,158,249,163,271,135,290,115,246,124" shape="poly">
+                        <area target="" data-id="4" title="4" href="" coords="260,197,252,163,291,113,362,109,345,137,320,163,291,182" shape="poly">
+                        <area target="" data-id="5" title="5" href="" coords="210,219,200,207,200,191,200,173,206,158,215,152,231,148,244,154,255,169,258,191,256,209,252,223,248,234,247,261,244,277,230,309,211,308,218,284,225,259,221,231" shape="poly">
+                        <area target="" data-id="6" title="6" href="" coords="260,198,258,207,265,214,279,215,298,214,310,215,332,217,349,219,364,214,380,211,396,197,402,187,403,178,395,168,387,159,330,154,299,178" shape="poly">
+                        <area target="" data-id="7" title="7" href="" coords="331,154,349,131,362,110,380,109,379,119,382,140,390,157" shape="poly">
+                        <area target="" data-id="8" title="8" href="" coords="406,177,414,157,415,139,405,110,398,91,390,80,383,97,381,133,391,161" shape="poly">
+                        <area target="" data-id="9" title="9" href="" coords="407,175,424,182,436,184,451,186,460,186,468,168,456,161,442,146,434,131,428,118,427,108,411,99,404,90,398,78,400,67,392,79,402,103,411,126,416,144,414,160" shape="poly">
+                        <area target="" data-id="10" title="10" href="" coords="430,108,446,111,456,112,458,135,463,156,466,167,447,152,434,131,426,114" shape="poly">
+                        <area target="" data-id="11" title="11" href="" coords="468,163,473,152,478,139,481,126,480,110,468,111,458,114,456,127,462,151" shape="poly">
+                        <area target="" data-id="12" title="12" href="" coords="424,53,438,55,452,63,465,77,475,95,480,110,447,111,415,103,400,84,399,69,407,62" shape="poly">
+                        <area target="" data-id="13" title="13" href="" coords="403,174,417,205,427,225,431,240,429,259,422,277,423,287,438,288,447,279,450,267,457,263,458,247,459,227,462,216,459,200,459,188,438,188,419,182" shape="poly">
+                    </map>
+                </div>
+                <div class="col-md-6">
+                    <div class="oink-detail">
+                        <h2>Cortes</h2>
+                        <div class="content"></div>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
@@ -202,18 +242,91 @@ get_header(); ?>
 
 <section class="mbr-section mbr-section__container" id="image2-t" style="background-color: rgb(255, 255, 255); padding-top: 0px; padding-bottom: 0px;">
     <div class="container">
-        <div class="row">
-            <div class="col-xs-12">
-                <figure class="mbr-figure">
-                    <div><img src="<?php echo DIR; ?>/assets/images/cortespollo-1400x452-62.png"></div>
-
-                    
-
-                </figure>
+        <div class="meat-module">
+            <div class="row">
+                <div class="col-md-6">
+                    <img width="540" height="350" src="<?php echo DIR; ?>/assets/images/chicken-plane.jpg" usemap="#chicken-map">
+                    <map name="chicken-map" id="chicken-map">
+                        <area target="" data-id="1" title="1" href="" coords="194,113,207,118,226,127,237,137,246,146,236,154,232,165,238,178,246,183,253,186,245,194,239,206,239,221,238,226,222,215,198,194,189,176,186,149,188,126" shape="poly">
+                        <area target="" data-id="2" title="2" href="" coords="240,226,239,212,241,203,246,192,255,187,265,190,274,197,277,218,278,232,275,240,270,243,251,234" shape="poly">
+                        <area target="" data-id="3" title="3" href="" coords="280,241,298,252,310,255,323,250,326,239,330,227,338,216,347,205,347,195,346,187,336,182,326,179,317,184,302,189,290,191,282,190,268,190,277,204,279,216,278,229" shape="poly">
+                        <area target="" data-id="4" title="4" href="" coords="323,178,328,164,324,151,308,143,282,139,256,143,243,150,238,155,234,166,237,174,244,181,259,187,280,188,305,188" shape="poly">
+                    </map>
+                </div>
+                <div class="col-md-6">
+                    <div class="chicken-detail">
+                        <h2>Cortes</h2>
+                        <div class="content"></div>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
 </section>
 
+
+
 <?php get_template_part("parts/bottom"); ?>
 <?php get_footer(); ?>
+<script src="<?php echo DIR; ?>/assets/rwdImageMaps/jquery.rwdImageMaps.js"></script>
+
+<script type="text/javascript">
+    var cowData = {
+        1:'<h3><span>1</span> Roast Beef</h3><p>Se cocina en estofados y guisos, de gran sabor y económico.</p>',
+        2:'<h3><span>2</span> Palomita y tortuguita</h3><p>De bajo tenor graso, buen sabor y precio atractivo, se utiliza para carne picada.</p>',
+        3:'<h3><span>3</span> Bife ancho</h3><p>Para preparar a la parrilla o plancha, la presencia del hueso en su cocción le da un gusto especial. Es el bife tradicional en platos populares. </p><div class="clearfix"></div><h3><span>3</span> Ojo de bife</h3><p>Es la parte central del bife ancho. En varios mercados del mundo y restaurantes argentinos de categoría, es considero el preferido entre los bifes por su sabor y terneza.</p>',
+        4:'<h3><span>4</span> Paleta</h3><p>Permite preparar churrascos de manera más económica así como estofados.</p>',
+        5:'<h3><span>5</span> Osobuco</h3><p>Favorito en la cocina argentina para pucheros y guisos. La gastronomía gourmet lo adoptó para platos en boga, valorizándolo.</p>',
+        6:'<h3><span>6</span> Bife de costilla</h3><p>Similar al bife ancho, es la parte de los bifes que se localiza en el cuarto trasero.</p><div class="clearfix"></div><h3><span>6</span> Bife de chorizo</h3><p>Emblemático de la carne argentina, es uno de los cortes favoritos para la parrilla. Constituye la parte central del bife angosto, con menos grasa y aprovechamiento integral.</p>',
+        7:'<h3><span>7</span> Entraña</h3><p>Especial para la parrilla. Su piel, crocante tras el grillado, es un gran atractivo.</p>',
+        8:'<h3><span>8</span> Asado</h3><p>Es el corte emblemático de la parrilla argentina. El contenido de grasa le da un sabor incomparable.</p><div class="clearfix"></div><h3><span>8</span> Matambre</h3><p>Corte dúctil, es aprovechado para arrollar con otros ingredientes y cocinar al  horno. También se prepara a la parrilla y tiernizado, como especialidad.</p>',
+        9:'<h3><span>9</span> Tapa de asado</h3><p>Corte apto para parrilla u horno, de gran conveniencia.</p>',
+        10:'<h3><span>10</span> Lomo</h3><p>Es el corte más apreciado por su terneza y bajo tenor graso. Valorado en la Argentina y en todos los mercados del mundo, es ideal para platos gourmet.</p>',
+        11:'<h3><span>11</span> Bife de lomo</h3><p>Es la parte más magra y tierna del vacuno. Se lo presenta en forma de medallón</p>',
+        12:'<h3><span>12</span> Vacío</h3><p>Uno de los tres cortes parrilleros preferidos por el consumidor  argentino, junto al asado y al matambre. Sirve también para hornear por su gran aprovechamiento.</p>',
+        13:'<h3><span>13</span> Cuadril</h3><p>Es el conjunto del corazón de cuadril y su tapa (picaña). De gran sabor, terneza y poca grasa, gran producto para la plancha o parrilla.</p><div class="clearfix"></div><h3><span>13</span> Colita de cuadril</h3><p>Corte del cuarto trasero, muy reconocido por su sabor y bajo contenido de grasa. El más elegido para preparar al horno, también permite su cocción en la parrilla y plancha.</p><div class="clearfix"></div><h3><span>13</span> Riñonada de cuadril</h3><p>Llamado también corazón de cuadril, es uno de los tres componentes del conjunto de cortes más valiosos de la exportación argentina, junto al lomo y al bife angosto sin hueso (bife de chorizo). Asada a la parrilla, resulta tierna y jugosa.</p><div class="clearfix"></div><h3><span>13</span> Picaña</h3><p>Es el nombre que se le da en algunos mercados a la tapa de cuadril. Favorito de la parrilla en Brasil y crecientemente elegido por los argentinos.</p>',
+        14:'<h3><span>14</span> Peceto</h3><p>Por su terneza y alto aprovechamiento, es el corte más selecto para milanesas. También, el clásico para el vitel tonné navideño.</p><div class="clearfix"></div><h3><span>14</span> Nalga</h3><p>Es la principal materia prima para milanesas, por su sabor y jugosidad, gran tamaño, poca presencia de grasa y nervios.</p><div class="clearfix"></div><h3><span>14</span> Cuadrada</h3><p>Para  milanesas y bifes a la criolla, es muy apreciada por las familias argentinas.</p><div class="clearfix"></div><h3><span>14</span> Tapa de nalga</h3><p>Permite su preparación al horno, rellena, mechada y en cacerola. Amplio aprovechamiento de un corte de precio conveniente.</p>',
+        15:'<h3><span>15</span> Bola de lomo</h3><p>Otro corte típico para milanesas, que también se utiliza para preparar bife a la criolla.</p>',
+        16:'<h3><span>16</span> Garrón</h3><p>Es la última extremidad de conjunto paleta. Se lo emplea hervido en guisos y sopas.</p>',
+    };
+    var oinkData = {
+        1:'<h3><span>1</span> Bondiola</h3><p>Su consumo ha crecido en popularidad; la preparación favorita es a la parrilla. Su terneza y gusto lo hacen muy apto para su consumo en sándwich.</p>',
+        2:'<h3><span>2</span> Carré con hueso</h3><p>Como el bife del vacuno, la preparación popular es el grillado</p>',
+        3:'<h3><span>3</span> Churrasquito</h3><p>Es el corte que cubre al costillar, equivalente a la tapa de asado. Se asa a la plancha o parrilla.</p>',
+        4:'<h3><span>4</span> Pechito de cerdo</h3><p>Es el equivalente al asado con vacío del vacuno. Se prepara preferentemente a la parrilla.</p>',
+        5:'<h3><span>5</span> Paleta</h3><p>La utilización más popular es para milanesas. Son muy tiernas y más económicas.</p><div class="clearfix"></div><h3><span>5</span> Chuleta de cerdo</h3><p>Es la paleta cortada en forma perpendicular al hueso. Se hacen unos churrascos tiernos y económicos.</p>',
+        6:'<h3><span>6</span> Matambrito</h3><p>Uno de los cortes preferidos para la parrilla. Su contenido graso explica su sabor típico. También se prepara tiernizado al horno.</p>',
+        7:'<h3><span>7</span> Solomillo</h3><p>Equivalente al lomo del vacuno, es el corte más valorado del cerdo por su terneza y poca grasa. Ideal para la parrilla y platos especiales.</p>',
+        8:'<h3><span>8</span> Bola de lomo</h3><p>La utilización más popular es para milanesas. Son muy tiernas y más económicas.</p>',
+        9:'<h3><span>9</span> Cuadrada</h3><p>La utilización más popular es para milanesas. Son muy tiernas y más económicas.</p>',
+        10:'<h3><span>10</span> Peceto</h3><p>La utilización más popular es para milanesas. Son muy tiernas y más económicas.</p>',
+        11:'<h3><span>11</span> Paleta</h3><p>La utilización más popular es para milanesas. Son muy tiernas y más económicas.</p>',
+        12:'<h3><span>12</span> Cuadril</h3><p>De gran terneza. La cocción preferida es poner la pieza completa a la parrilla.</p>',
+        13:'<h3><span>13</span> Patitas</h3><p>Se las suele preparar al escabeche, guisadas o para enriquecer salsas estofadas.</p>',
+    };
+    var chickenData = {
+        1:'<h3><span>1</span> Pechugas</h3><p>Las Supremas son las pechugas sin hueso, que se consumen cocinadas a la plancha o en forma de milanesas. Es un corte 100% de carne de alta calidad, sin desperdicios.</p>',
+        2:'<h3><span>2 & 3</span> Pata con muslo</h3><p>Se cocinan a la plancha o en horno. Su piel, grasa y huesos le dan un gran sabor. Preferidos por los niños.</p>',
+        3:'<h3><span>2 & 3</span> Pata con muslo</h3><p>Se cocinan a la plancha o en horno. Su piel, grasa y huesos le dan un gran sabor. Preferidos por los niños.</p>',
+        4:'<h3><span>4</span> Alas</h3><p>Su textura la asemeja a un muslo pequeño, es jugosa y hoy está presente en muchas recetas deliciosas.</p>',
+    };
+    $(document).ready(function(e) {
+        $('map').imageMapResize();
+        $('#cow-map area').on('click', function(e) {
+            e.preventDefault();
+            var id = $(this).attr('data-id');
+            $('.meat-module .cow-detail .content').html(cowData[id]);
+        });
+        $('#chicken-map area').on('click', function(e) {
+            e.preventDefault();
+            var id = $(this).attr('data-id');
+            $('.meat-module .chicken-detail .content').html(chickenData[id]);
+        });
+        $('#oink-map area').on('click', function(e) {
+            e.preventDefault();
+            var id = $(this).attr('data-id');
+            $('.meat-module .oink-detail .content').html(oinkData[id]);
+        });
+    });
+</script>
+
