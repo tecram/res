9b2154c10a9d29ae5c734f024d1a3d2245b6e273
tecram <marcet_cristian@yahoo.com.ar>
2017-06-12 12:31:16 -0300

    - Trabaja con nosotros link

--- cms/wp-content/themes/res/parts/bottom.php
+++ cms/wp-content/themes/res/parts/bottom.php
@@ -46,10 +46,10 @@ global $youtube;
   <div class="col-md-4">
     <div class="box-module row">
       <div class="text-center">
-        <h3><strong>HACEMOS<br>ESCUELA</strong></h3>
+        <h3><a href="/contacto#content1-17"><strong>HACEMOS<br>ESCUELA</strong></a></h3>
       </div>
       <div class="text-center">
-        <p>Trabaja con nosotros</p>
+        <p><a href="/contacto#content1-17">Trabaja con nosotros</a></p>
       </div>
       <img src="<?php echo DIR; ?>/assets/images/trabaja.jpg" alt="" class="bg">
     </div>
