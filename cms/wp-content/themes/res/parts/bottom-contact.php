af965869eab67256d1132cd2d7685d8e8deaa13f
tecram <marcet_cristian@yahoo.com.ar>
2017-06-14 11:16:46 -0300

    - Change links

--- cms/wp-content/themes/res/parts/bottom-contact.php
+++ cms/wp-content/themes/res/parts/bottom-contact.php
@@ -15,10 +15,10 @@ global $youtube;
   <div class="col-md-4">
     <div class="box-module row">
       <div class="text-center">
-        <h3><a href="/locales#form2-2j"><strong>ACÁ<br>ESTAMOS</strong></h3></h3>
+        <h3><a href="/nosotros"><strong>ACÁ<br>ESTAMOS</strong></h3></h3>
       </div>
       <div class="text-center">
-        <p><a href="/locales#form2-2j">Encontrá tu sucursal</a></p>
+        <p><a href="/nosotros">Encontrá tu sucursal</a></p>
       </div>
       <img src="<?php echo DIR; ?>/assets/images/aca-estamos.jpg" alt="" class="bg">
     </div>
